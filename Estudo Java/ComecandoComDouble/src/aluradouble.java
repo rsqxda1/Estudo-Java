1d8263b52538ad36a0fc57e0c7ed3e455a04d3ff
Renata <rsqxda86ti@gmail.com>
2021-11-13 15:09:28 -0300

    testando repositorio

---
+++ Estudo Java/ComecandoComDouble/src/aluradouble.java
@@ -0,0 +1,19 @@
+
+public class aluradouble {
+	
+	public static void main(String[] args) {
+		 double salario;
+		salario = 1200.10;
+		//int valor =
+		
+		
+		System.out.println("meu salario é " + salario);
+		
+		double idade = 35;
+		
+		
+		double dividir = 3.14 / 2;
+		System.out.println("a divisao é " + dividir);
+	}
+
+}
